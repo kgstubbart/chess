6e3c70a570cf17537f64aaf355274fb7d9bfc6b1
Kason Stubbart <kgstubbart@gmail.com>
2024-09-18 21:18:53 -0600

    created base movements for pieces

--- shared/src/main/java/chess/movement/BaseMovementRule.java
+++ shared/src/main/java/chess/movement/BaseMovementRule.java
@@ -11,10 +11,26 @@ public abstract class BaseMovementRule implements PieceMovementCalculator{
     protected void calculateMoves(ChessBoard board, ChessPosition pos, int rowInc, int colInc,
                                   Collection<ChessMove> moves, boolean allowDistance) {
 
-        // Generic code for calculating most piece rules
-        // ...
+        int row = pos.getRow();
+        int col = pos.getColumn();
+
+        if (allowDistance) {
+            while (row >= 1 && row <= 8 && col >= 1 && col <= 8) {
+                int updated_row = row + rowInc;
+                int updated_col = col + colInc;
+                ChessPosition position = new ChessPosition(updated_row, updated_col);
+                if (board.getPiece(position) != null) {
+                    if (board.getPiece(position).getTeamColor() != board.getPiece(pos).getTeamColor()) {
+                        moves.add(new ChessMove(pos, position, null));
+                    }
+                    break;
+                }
+                else {
+                    moves.add(new ChessMove(pos, position, null));
+                }
+            }
+        }
     }
 
     public abstract Collection<ChessMove> moves(ChessBoard board, ChessPosition position);
 }
-
