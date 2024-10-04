c936283fdb373e790ee630e1383a40b5813fc914
Kason Stubbart <kgstubbart@gmail.com>
2024-10-03 18:21:58 -0600

    set up basic instruction for BaseDanger

--- shared/src/main/java/chess/KingDanger.java
+++ shared/src/main/java/chess/KingDanger.java
@@ -1,7 +1,26 @@
 package chess;
 
 public class KingDanger {
-    public KingDanger(ChessBoard board, ChessPosition pos) {
+    private final ChessBoard board;
+    private final ChessPosition pos;
+    private final ChessGame.TeamColor teamColor;
 
+    public KingDanger(ChessBoard board, ChessPosition pos, ChessGame.TeamColor teamColor) {
+        this.board = board;
+        this.pos = pos;
+        this.teamColor = teamColor;
+    }
+
+    protected boolean BaseDanger(ChessBoard board, ChessPosition pos, ChessGame.TeamColor teamColor) {
+        // check for king (may be able to do pawn here too)
+
+        // check for pawn
+
+        // check for knight
+
+        // check diagonal
+
+        // check straight
+        return false;
     }
 }
