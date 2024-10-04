addbbb9f75729fc7c65a7270012554356d38c431
Kason Stubbart <kgstubbart@gmail.com>
2024-10-04 15:54:24 -0600

    added row and col switch

--- shared/src/main/java/chess/KingDanger.java
+++ shared/src/main/java/chess/KingDanger.java
@@ -15,15 +15,41 @@ public class KingDanger {
         int row = pos.getRow();
         int col = pos.getColumn();
 
-        // check for king (may be able to do pawn here too)
+        int temp_col = col + 1;
+        int temp_row = switch (row) {
+            case 7 -> 1;
+            case 6 -> 2;
+            case 5 -> 3;
+            case 4 -> 4;
+            case 3 -> 5;
+            case 2 -> 6;
+            case 1 -> 7;
+            case 0 -> 8;
+            default -> row;
+        };
 
-        // check for pawn
+        if (teamColor == ChessGame.TeamColor.WHITE) {
+            // check for king (may be able to do pawn here too)
 
-        // check for knight
+            // check for pawn
 
-        // check diagonal
+            // check for knight
 
-        // check straight
+            // check diagonal
+
+            // check straight
+        }
+        else {
+            // check for king (may be able to do pawn here too)
+
+            // check for pawn
+
+            // check for knight
+
+            // check diagonal
+
+            // check straight
+        }
         return false;
     }
 }
