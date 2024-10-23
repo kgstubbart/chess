bac8d6b5f2386d86a9002e3a865945dcd9d678e2
Kason Stubbart <kgstubbart@gmail.com>
2024-10-23 16:46:06 -0600

    server createUser

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,8 +1,18 @@
 package server;
 
+import com.google.gson.Gson;
+import dataaccess.DataAccess;
+import dataaccess.MemoryDataAccess;
+import model.UserData;
 import spark.*;
+import service.Service;
+
+import java.util.Map;
 
 public class Server {
+    private final DataAccess dataAccess = new MemoryDataAccess();
+    private final Service service = new Service(dataAccess);
+    private final Gson serializer = new Gson();
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
@@ -10,12 +20,24 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
-
-        //This line initializes the server and can be removed once you have a functioning endpoint 
-        Spark.init();
+        Spark.post("/user", this::createUser);
+        Spark.exception(Exception.class, this::exceptionHandler);
+        //This line initializes the server and can be removed once you have a functioning endpoint
 
         Spark.awaitInitialization();
         return Spark.port();
+    }
+
+    private String createUser(Request request, Response response) throws Exception {
+        var newUser = serializer.fromJson(request.body(), UserData.class);
+        var result = service.registerUser(newUser);
+        return serializer.toJson(result);
+    }
+
+    private void exceptionHandler(Exception exception, Request request, Response response) {
+        response.status(500);
+        response.body(serializer.toJson(Map.of("message", exception.getMessage())));
+        exception.printStackTrace(System.err);
     }
 
     public void stop() {
