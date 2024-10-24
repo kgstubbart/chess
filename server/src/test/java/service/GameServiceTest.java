78ce4b88a4261f36a2fcce23eb23d64292470f02
Kason Stubbart <kgstubbart@gmail.com>
2024-10-24 16:28:54 -0600

    positive registerUser unit test

--- server/src/test/java/service/GameServiceTest.java
+++ server/src/test/java/service/GameServiceTest.java
@@ -1,4 +1,25 @@
 package service;
 
-public class GameServiceTest {
+import dataaccess.MemoryAuthDataAccess;
+import dataaccess.MemoryGameDataAccess;
+import org.junit.jupiter.api.Test;
+
+class GameServiceTest {
+    static final GameService service = new GameService(new MemoryGameDataAccess(), new MemoryAuthDataAccess());
+
+    @Test
+    void createGame() {
+    }
+
+    @Test
+    void joinGame() {
+    }
+
+    @Test
+    void listGames() {
+    }
+
+    @Test
+    void clearGames() {
+    }
 }
--- server/src/test/java/service/UserServiceTest.java
+++ server/src/test/java/service/UserServiceTest.java
@@ -1,4 +1,51 @@
 package service;
 
-public class UserServiceTest {
+import dataaccess.MemoryAuthDataAccess;
+import dataaccess.MemoryUserDataAccess;
+import model.AuthData;
+import model.UserData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+    static final UserService service = new UserService(new MemoryUserDataAccess(), new MemoryAuthDataAccess());
+
+    @BeforeEach
+    void clear() throws ServiceException {
+        service.clearUsers();
+    }
+
+    @Test
+    void positive_registerUser() throws ServiceException {
+        UserData userData = new UserData("apple", "banana", "pear@mail");
+        AuthData authData = service.registerUser(userData);
+
+        assertEquals(userData.username(), authData.username());
+        assertNotNull(authData.authToken());
+    }
+
+    @Test
+    void negative_registerUser() {
+    }
+
+    @Test
+    void positive_loginUser() {
+    }
+
+    @Test
+    void negative_loginUser() {
+    }
+
+    @Test
+    void positive_logoutUser() {
+    }
+
+    @Test
+    void negative_logoutUser() {
+    }
+
+    @Test
+    void clearUsers() {
+    }
 }
