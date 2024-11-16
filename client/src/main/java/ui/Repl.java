03be89ac9da8a5c1e9c06da9f4fcffdb40c78730
Kason Stubbart <kgstubb@byu.edu>
2024-11-16 16:55:01 -0700

    starter code Repl

---
+++ client/src/main/java/ui/Repl.java
@@ -0,0 +1,39 @@
+package ui;
+
+import ui.EscapeSequences;
+import java.util.Scanner;
+
+public class Repl {
+    private final PetClient client;
+
+    public Repl(String serverUrl) {
+        client = new PetClient(serverUrl, this);
+    }
+
+    public void run() {
+        System.out.println("\uD83D\uDC36 Welcome to the pet store. Sign in to start.");
+        System.out.print(client.help());
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                result = client.eval(line);
+                System.out.print(BLUE + result);
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+        System.out.println();
+    }
+
+
+    private void printPrompt() {
+        System.out.print("\n" + RESET + ">>> " + GREEN);
+    }
+
+}
--- client/src/main/java/ui/State.java
+++ client/src/main/java/ui/State.java
@@ -1,6 +1,6 @@
 package ui;
 
 public enum State {
-  LOGGEDOUT,
-  LOGGEDIN
+    LOGGEDOUT,
+    LOGGEDIN
 }
